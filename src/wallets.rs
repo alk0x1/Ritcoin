7beed9e2249cdebf9f096a05532abbb743c11214
alk0051 <alanbertani051@gmail.com>
2022-11-28 20:54:29 -0300

    add: add wallets on file

--- src/wallets.rs
+++ src/wallets.rs
@@ -1,7 +1,8 @@
-use rsa::pkcs1v15;
 use rsa::pkcs8::der::zeroize::Zeroizing;
 use rsa::pkcs8::{EncodePublicKey, DecodePublicKey, EncodePrivateKey, DecodePrivateKey};
-use rsa::{RsaPrivateKey, RsaPublicKey, pkcs1::DecodeRsaPublicKey};
+use rsa::{RsaPrivateKey, RsaPublicKey};
+use std::fs::File;
+use std::io::prelude::*;
 
 pub fn create_wallet() {
   let mut rng = rand::thread_rng();
@@ -9,15 +10,12 @@ pub fn create_wallet() {
   let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
   let pub_key = RsaPublicKey::from(&priv_key);
 
-
   let (encoded_public_key, encoded_private_key) = encode_keys_pkcs8(pub_key.clone(), priv_key.clone());
   let (decoded_public_key, decoded_private_key) = decode_keys_pkcs8(encoded_public_key.clone(), encoded_private_key.clone());
-
-  println!("Public Key: {:?}\nSecret Key: {:?}", encoded_public_key, encoded_private_key.as_str());
-
   assert_eq!(pub_key, decoded_public_key);
   assert_eq!(priv_key, decoded_private_key);
-  println!("passou")
+
+  save_keys_on_file(encoded_public_key, encoded_private_key);
 }
 
 pub fn encode_keys_pkcs8(pubkey: RsaPublicKey, privkey: RsaPrivateKey) -> (String, Zeroizing<String>) {
@@ -32,4 +30,11 @@ pub fn decode_keys_pkcs8(pubpem: String, privpem: Zeroizing<String>) -> (RsaPubl
   let private_key = RsaPrivateKey::from_pkcs8_pem(&privpem).unwrap();
 
   (public_key, private_key)
+}
+
+pub fn save_keys_on_file(pubkey: String, privkey: Zeroizing<String>) {
+  let mut pub_file = File::create("./wallets/public.txt").unwrap();
+  let mut priv_file = File::create("./wallets/secret.txt").unwrap();
+  pub_file.write_all(pubkey.as_bytes()).unwrap();
+  priv_file.write_all(privkey.as_bytes()).unwrap();
 }
